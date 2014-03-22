3a64d694a7f872c971ca31f7dea2060dc5c23085
Davide <thekage91@gmail.com>
2014-03-22 10:15:07 +0100

    Add inter wall

--- 2014-03-21/python/exercise1.py
+++ 2014-03-21/python/exercise1.py
@@ -1,22 +1,34 @@
 
 from pyplasm import *
 
+wall0 = PROD([QUOTE([10]) , QUOTE([10])])
+internal0 = T([1,2])([0.5,0.5])(PROD([QUOTE([9]) , QUOTE([9])]))
+floor0 = STRUCT([wall0 , COLOR(BROWN)(internal0)])
 
-floor0 = PROD([QUOTE([10]) , QUOTE([10])])
+wall1 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.90,0.90])(wall0))
+internal1 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.90,0.90])(internal0))
+floor1 = STRUCT([wall1 , COLOR(RED)(internal1)])
 
-floor1 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.90,0.90])(floor0))
+wall2 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.9,0.9])(wall1))
+internal2 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.90,0.90])(internal1))
+floor2 = STRUCT([wall2 , COLOR(YELLOW)(internal2)])
 
-floor2 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.9,0.9])(floor1))
+wall3 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.9,0.9])(wall2))
+internal3 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.90,0.90])(internal2))
+floor3 = STRUCT([wall3 , COLOR(GREEN)(internal3)])
 
-floor3 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.9,0.9])(floor2))
+wall4 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.9,0.9])(wall3))
+internal4 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.90,0.90])(internal3))
+floor4 = STRUCT([wall4 , COLOR(ORANGE)(internal4)])
 
-floor4 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.9,0.9])(floor3))
+wall5 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.9,0.9])(wall4))
+internal5 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.90,0.90])(internal4))
+floor5 = STRUCT([wall5 , COLOR(BLACK)(internal5)])
 
-floor5 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.9,0.9])(floor4))
+wall6 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.9,0.9])(wall5))
+internal6 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.90,0.90])(internal5))
+floor6 = STRUCT([wall6 , COLOR(CYAN)(internal6)])
 
-floor6 = T([1,2,3])([0.45,0.45,2])(S([1,2])([0.9,0.9])(floor5))
-
-floors = STRUCT([floor0 , COLOR(RED)(floor1) , COLOR(YELLOW)(floor2) , COLOR(GREEN)(floor3) , 
-					COLOR(ORANGE)(floor4) , COLOR(WHITE)(floor5) , COLOR(CYAN)(floor6)])
+floors = STRUCT([floor0 , floor1 , floor2 , floor3 , floor4 , floor5 , floor6])
 
 VIEW(floors)
