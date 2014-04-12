eab109b48b28f56af4901e67d4776fd255805147
Davide <thekage91@gmail.com>
2014-04-12 11:59:50 +0200

    Perform exercise 3 with new building

--- 2014-04-11/python/exercise3.py
+++ 2014-04-11/python/exercise3.py
@@ -23,6 +23,8 @@ field = COLOR([0.164,0.37,0.168])(CUBOID([50,50]))
 # Creazione base edificio 1
 # base_b1 = base building 1
 # base_bx = base building x
+#
+# Vedere in /images/fig0.png il modello di riferimento
 #####################################################
 
 base1 = CUBOID([14,14])
@@ -74,9 +76,7 @@ modWall1D = larModelProduct([modelEdges , modelWall])
 
 structure_building = STRUCT(AA(COLOR(GRAY))(MKPOLS((mod1D))) + MKPOLS((mod2D)) + MKPOLS((modWall1D)))
 structure_building = COLOR([0.333 , 0.333 , 0.345])(structure_building)
-#building1 = ([1,2])([30,30])(STRUCT([base_b1 , field]))
-#building1 = STRUCT([fondamenta , base_b1, field])
-#VIEW(building1)
+#VIEW(structure_building)
 
 #####################################################
 # Crezione facciata edificio
@@ -115,16 +115,128 @@ structure_building = STRUCT([structure_building ,facade , facade_2 , facade_3 , 
 structure_building = T([1,2,3])([2,2,4])(structure_building)
 
 building1 = STRUCT([structure_building , base_final])
+building1 = T([1,2])([30,30])(building1)
 
-VIEW(building1)
+#VIEW(building1)
 
 #####################################################
 # Creazione edificio 2
+# Vedere in /images/fig1.png il modello di riferimento
+#####################################################
+
+#####################################################
+# Creazione base
+#####################################################
+
+base1_b2 = CUBOID([8,6])
+base2_b2 = CUBOID([7.5,5.5])
+
+base2_b2 = T([1,2])([0.25,0.25])(PROD([base2_b2 , Q(0.25)]))
+base2_b2 = COLOR(WHITE)(base2_b2)
+
+#base_b2 = STRUCT([base2_b2 , base1_b2])
+
+colonna = larRod([.1,2.5])([32,1])
+colonna = STRUCT(MKPOLS(colonna))
+colonna = T([1,2,3])([1,1,0.25])(colonna)
+
+colonna2 = T(2)(4)(colonna)
+colonna3 = T(1)(6)(colonna)
+colonna4 = T(2)(4)(colonna3)
+
+base_b2 = STRUCT([base2_b2 , base1_b2 , colonna , colonna2 , colonna3 , colonna4])
+#VIEW(base_b2)
+
+
+#####################################################
+#####################################################
+
+#####################################################
+# Creazione piani
+# viene creato il primo piano e poi replicato
 #####################################################
 
 
+def create_bulding_floors(floor):
+	floors = []
+	f1 = floor
+	for i in range(1,3):
+		f1 = T(3)(2.75)(f1)
+		floors.append(f1)
+	return floors
 
 
+
+f1 = CUBOID([8,6])
+f1 = COLOR(BLACK)(PROD([f1 , Q(0.2)]))
+
+f_int = CUBOID([7,5])
+f_int = T([1,2])([0.25,0.25])(PROD([f_int , Q(1.5)]))
+
+f_ext = CUBOID([7.5,5.5])
+f_ext = PROD([f_ext , Q(1.5)])
+
+f_t = COLOR(WHITE)(T([1,2])([0.25,0.25])(DIFFERENCE([f_ext , f_int])))
+
+colonne = STRUCT([colonna,colonna3,colonna2,colonna4])
+
+floor = STRUCT([f1 , f_t , colonne])
+
+building2_floors = STRUCT(create_bulding_floors(floor))
+
+#VIEW(building_floors)
+
+
+#####################################################
+# Creazione finestre
+#####################################################
+
+w1 = CUBOID([1,1.25])
+w2 = CUBOID([0.9,1.15])
+
+w1 = COLOR(GRAY)(PROD([w1 , Q(0.2)]))
+w2 = T([1,2])([0.05,0.05])(w2)
+w2 = COLOR(WHITE)(PROD([w2 , Q(0.2)]))
+
+window =  STRUCT([w1 , w2])
+window = R([2,3])(PI/2)(window)
+
+windows = STRUCT([window , T(1)(1)(window)])
+#VIEW(windows)
+
+#####################################################
+#####################################################
+
+
+#####################################################
+# Creazione tetto
+#####################################################
+
+top = SCALE(3)(0.7)(STRUCT([f_t ,f1]))
+top = T(3)(8.25)(top)
+
+#####################################################
+# Assemblamento edificio
+#####################################################
+
+windows = T([1,2,3])([1.2,0.45,4.25])(windows)
+
+pair_x = [T(1)(2) , windows]
+windows_final_1 = STRUCT(NN(2)(pair_x))
+
+windows_final_2 = T([1,3])([-1,2.75])(windows_final_1)
+windows_final_3 = T(2)([5.25])(windows_final_2)
+windows_final_4 = T(2)([5.25])(windows_final_1)
+
+windows_final_5 = T(1)(0.75)(R([1,2])(PI/2)(windows))
+windows_final_6 = T([2,3])([1,2.75])((windows_final_5))
+
+windows_building_final = STRUCT([windows_final_1 , windows_final_2 , windows_final_3 , windows_final_4 , windows_final_5,
+								windows_final_6])
+
+building2 = STRUCT([building2_floors , base_b2 , top , windows_building_final])
+
+VIEW(building2)
 
 
 
