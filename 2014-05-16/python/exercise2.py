6581a39fa46fa90e2ebadb71d2ca9f8612a48cf4
Davide <thekage91@gmail.com>
2014-05-17 12:48:41 +0200

    insert curve in exercise 2

--- 2014-05-16/python/exercise2.py
+++ 2014-05-16/python/exercise2.py
@@ -27,6 +27,10 @@ def view_numerating_cells(master):
 	hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
 	VIEW(hpc)
 
+def inserts_elements(master , diagram , cells):
+	for i,item in enumerate(cells):
+		diagram = diagram2cell(master , diagram , item)
+	return diagram
 
 DRAW = COMP([STRUCT,MKPOLS])
 DRAW_SKEL = COMP([SKEL_1,STRUCT,MKPOLS])
@@ -236,7 +240,7 @@ toMerge = 17
 shape_r9 = [1,3,2]
 dimesione_r9 = [[0.3] ,[1.58,0.92,1.5], [2.2,0.8]]
 
-view_numerating_cells(master)
+#view_numerating_cells(master)
 
 cell = MKPOL([master[0],[[v+1 for v in  master[1][toMerge]]],None])
 #VIEW(STRUCT([appartament,cell]))
@@ -283,74 +287,100 @@ master = removes_elements(master , toRemove)
 # Esercizio 2
 ####################################
 
-
-floor1 = DRAW(master)
-floor2 = T([3])([3.3])(floor1)
-floor3 = T([3])([3.3])(floor2)
-
-condominio = STRUCT([floor1 , floor2 , floor3])
-
-
 stair = spiralStair(nturns = 10)
 stair = DRAW(stair)
 stair = T([1,2])([-1,6.5])(stair)
-condominio_all = STRUCT([stair , condominio])
-VIEW(condominio_all)
+#condominio_all = STRUCT([stair , condominio])
+#VIEW(condominio_all)
 
 
+#####################
+# Inserimento piani
+#####################
+
+condominio = assemblyDiagramInit([1,1,8])([[12.3],[10],[4,0.4]*4])
+#VIEW(DRAW(condominio))
+#view_numerating_cells(condominio)
+
+condominio =  inserts_elements(master,condominio,[2,3,4])
+#VIEW(DRAW(condominio))
 
 ####################################
 # Costruzione base
 ####################################
 
-
-shape = [5,5,1]
-dimension = [[1,4.5,1,4.5,1] , [1,2.5,1,2.5,1] , [3]]
+shape = [3,1,1]
+dimension = [[1.5,9,1.5] , [10] , [4]]
 
 master_b = assemblyDiagramInit(shape)(dimension)
-V,CV = master_b
-hpc = SKEL_1(STRUCT(MKPOLS(master_b)))
-hpc = cellNumbering (master_b,hpc)(range(len(CV)),CYAN,2)
-
-master_b = select_elements((master_b) , [0,2,4,10,12,14,20,22,24])
-hpc = DRAW(master_b)
-
-builds = STRUCT([hpc , stair , T(3)(3)(condominio)])
-#VIEW(builds)
+condominio = inserts_elements(master_b,condominio,[0])
+condominio = removes_elements(condominio , [299])
+#VIEW(DRAW(condominio))
 
 
-condominio2 = T(2)(16)(condominio_all)
-condominio3 = T(2)(16)(condominio2)
-
-final_build = STRUCT([condominio_all , condominio2 , condominio3])
-VIEW(final_build)
+condominio = STRUCT([ T([2])([12])(DRAW(condominio)) , DRAW(condominio)])
+#VIEW(condominio)
 
 ####################################
 # Costruzione curve
 ####################################
 
-domain = larDomain([32])
-controlPoints = [[0,0,0],[.4,0,0.05],[.05,0,.25],[.2,0,.3]]
-profile = BEZIER(S1)(controlPoints)
-domain = EMBED(1)(PROD([ Hpc(Grid([10*[1./10]])),  Hpc(Grid([30*[2*PI/30]])) ]))
-mapping = ROTATIONALSURFACE(profile)
-vaso1 = COLOR(ORANGE)(MAP(mapping)(domain))
-VIEW(vaso1)
+controls = [[2,0,0] , [2,0,1] , [3,0,2] , [4,0,3]]
+controls1 = [[4,0,3] , [5,0,2] , [6,0,1] , [6,0,0]]
+#controls2 = [[1,0,0] , [1,0,0] , [2,0,1] , [3,0,2]]
+#controls3 = [[3,0,2] , [4,0,1] , [5,0,0] , [5,0,0]]
 
+b1 = BEZIER(S1)(controls)
+b2 = BEZIER(S1)(controls1)
 
 domain = larDomain([48,48])
-controlPoints1 = [[0,0,0],[40,0,-40],[50,0,-50]]
-controlPoints2 = [[0,50.5,0],[40,50.5,20],[50,50.5,-30]]
-controlPoints3 = [[0,70,0],[40,70,-10],[50,70,-30]]
+mapping2 = BEZIER(S2)([b1,b2])
+surface2 = larMap(mapping2)(domain)
+surface_curva = COLOR(RED)(STRUCT(MKPOLS(surface2)))
+#VIEW(surface_curva)
 
-b1 = BEZIER(S1)(controlPoints1)
-b2 = BEZIER(S1)(controlPoints2)
-b3 = BEZIER(S1)(controlPoints3)
 
-mapping = BEZIER(S2)([b1,b2,b3])
-surface = larMap(mapping)(domain)
-model = STRUCT(MKPOLS(surface))
-VIEW(model)
+#condominio = STRUCT([condominio , surface_curva])
+#VIEW(condominio)
 
+
+"""Costruzione Vaso"""
+
+def rotationalSurface2(profile):
+	domain = EMBED(1)(PROD([ Hpc(Grid([10*[1./10]])),  Hpc(Grid([30*[2*PI/30]])) ]))
+	mapping = ROTATIONALSURFACE(profile)
+	obj = MAP(mapping)(domain)
+	return obj
+
+def create_profile(controlPoints):
+	b1 = BEZIER(S1)(controlPoints[0])
+	b2 = BEZIER(S1)(controlPoints[1])
+	profile = BEZIER(S1)([b1,b2])
+	return profile
+
+domain = larDomain([32])
+controlPoints = [[[0.5, 0.0, 0.0], [0.25, 0.0, 0.0], [0.25, 0.0, 0.25]], [[0.25, 0.0, 0.25], [0.25, 0.0, 0.75], [0.5, 0.0, 0.75]], [[0.5, 0.0, 0.75], [0.5, 0.0, 1.0], [0.75, 0.0, 1.0]]]
+
+profile = create_profile(controlPoints)
+obj2 = rotationalSurface2(profile)
+
+vaso1 = COLOR(RED)(obj2)
+vaso2 = COLOR(RED)(T([1,2])([9,9])(vaso1))
+vaso3 = COLOR(RED)(T([1,2])([3,9])(vaso1))
+
+terreno = COLOR(GREEN)(CUBOID([30,30]))
+condominio = STRUCT([condominio , vaso2 , vaso3 , terreno])
+
+
+"""Vaso 2"""
+
+controlPoints = [[0,0,0],[2,0,0.5],[.5,0,1.5],[2,0,2]]
+profile = BEZIER(S1)(controlPoints)
+obj3 = rotationalSurface2(profile)
+
+vaso4 = COLOR(ORANGE)(T([1,2])([4,4])(obj3))
+condominio = STRUCT([condominio , vaso2 , vaso3 , vaso4 , terreno])
+
+VIEW(condominio)
 
 
