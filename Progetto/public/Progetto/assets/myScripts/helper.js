57e71de68a1a843a4fd04cc233a89143d3599b33
Davide <thekage91@gmail.com>
2014-06-16 13:26:57 +0200

    Added obj model

--- Progetto/public/Progetto/assets/myScripts/helper.js
+++ Progetto/public/Progetto/assets/myScripts/helper.js
@@ -1,14 +1,15 @@
 
 
-function loadObjModel(obj_name , texture_name){
+function loadObjModel(obj_name , texture_name , scene , options){
 
-  var obj_model = null;
+   obj_model = null;
   var loader = new THREE.OBJLoader();
   loader.load("assets/obj_models/" + obj_name, function (obj) {
 
     var texture = THREE.ImageUtils.loadTexture("assets/textures/" + texture_name);
-    var material = new THREE.MeshPhongMaterial();
-    material.map = texture;
+    var material = new THREE.MeshLambertMaterial({color: 0xffffff, shading: THREE.FlatShading});
+    //var material = new THREE.MeshPhongMaterial();
+    //material.map = texture;
 
     obj.traverse(function (child) {
       if (child instanceof THREE.Mesh) {
@@ -17,7 +18,9 @@ function loadObjModel(obj_name , texture_name){
     });
 
     obj_model = obj;
+    obj.scale.set(20, 20, 20);
+    obj.position.set(10,10,10);
+    obj.rotation.x = -0.3;
+    scene.add(obj);
   });
-
-  return obj_model;
 }
